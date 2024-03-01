d710785ad3e3c018c5ece1d4f9e5569ca3b78ea5
Smit2202 <smit.bhanushali@arishti.com>
2024-03-01 15:26:15 +0530

    New Task

--- QueueLearning/Program.cs
+++ QueueLearning/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using QueueLearning;
+
+namespace QueueLearning
+{
+    class QueueLearning
+    {
+        public static void Main(string[] args)
+        {
+            // create a queue
+            Queue<string> fruits = new Queue<string>();
+            bool Task = false;
+
+            while (!Task)
+            {
+                Console.WriteLine($"{Environment.NewLine}Add = A, {Environment.NewLine}Displaying the Queue = B,{Environment.NewLine}Check = F, {Environment.NewLine}Quit = E");
+                Console.WriteLine();
+
+                Console.Write("Enter What You Want to do? : ");
+                string Uinput = Console.ReadLine();
+                switch (Uinput)
+                {
+                    case "A":
+                        // adds
+                        Console.Write("Enter the string: ");
+                        string Uin = Console.ReadLine();
+                        QueueAdd.Add(Uin, fruits);
+                        break;
+                    case "B":
+                        // print elements of the queue 
+                        foreach (string item in fruits)
+                        {
+                            Console.WriteLine(item);
+                        }
+                        break;
+                    case "E":
+                        Task = true;
+                        break;
+                    case "F":
+                        Console.WriteLine("Enter String");
+                        string nstring = Console.ReadLine(); 
+                        Console.WriteLine(fruits.Contains(nstring));
+                        break;
+                    default:
+                        Console.WriteLine("Enter Valid Inpute");
+                        break;
+                }
+
+            }
+
+            
+
+        }
+    }
+}
---
+++ QueueLearning/QueueAdd.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueLearning
+{
+    public class QueueAdd
+    {
+        public static void Add(string item, Queue<string> QNAme)
+        {
+            QNAme.Enqueue(item);
+        }
+    }
+}
